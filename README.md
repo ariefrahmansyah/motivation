74016d9aaeba589d15da60a7252cff4b1cefbe9e
ariefrahmansyah <ariefrahmansyah@hotmail.com>
2021-08-03 12:54:44 +0700

    First commit :rocket:

---
+++ README.md
@@ -0,0 +1,20 @@
+# Motivation
+
+A modified version of [Motivation](https://chrome.google.com/webstore/detail/motivation/ofdgfpchbidcgncgfpdlpclnpaemakoj) Chrome Extension.
+
+What's the difference? Well, I removed the `age` label. See screenshots below.
+
+**Before**
+
+![Before](/images/before.png)
+
+**After**
+
+![After](/images/after.png)
+
+## How to install
+
+1. Clone this repository
+2. Go to [chrome://extensions](chrome://extensions)
+3. Click `Load unpacked` button
+4. Load this repository
---
+++ app/app.js
@@ -0,0 +1,84 @@
+(function(){
+
+var $  = document.getElementById.bind(document);
+var $$ = document.querySelectorAll.bind(document);
+
+var App = function($el){
+  this.$el = $el;
+  this.load();
+
+  this.$el.addEventListener(
+    'submit', this.submit.bind(this)
+  );
+
+  if (this.dob) {
+    this.renderAgeLoop();
+  } else {
+    this.renderChoose();
+  }
+};
+
+App.fn = App.prototype;
+
+App.fn.load = function(){
+  var value;
+
+  if (value = localStorage.dob)
+    this.dob = new Date(parseInt(value));
+};
+
+App.fn.save = function(){
+  if (this.dob)
+    localStorage.dob = this.dob.getTime();
+};
+
+App.fn.submit = function(e){
+  e.preventDefault();
+
+  var input = this.$$('input')[0];
+  if ( !input.valueAsDate ) return;
+
+  this.dob = input.valueAsDate;
+  this.save();
+  this.renderAgeLoop();
+};
+
+App.fn.renderChoose = function(){
+  this.html(this.view('dob')());
+};
+
+App.fn.renderAgeLoop = function(){
+  this.interval = setInterval(this.renderAge.bind(this), 100);
+};
+
+App.fn.renderAge = function(){
+  var now       = new Date
+  var duration  = now - this.dob;
+  var years     = duration / 31556900000;
+
+  var majorMinor = years.toFixed(9).toString().split('.');
+
+  requestAnimationFrame(function(){
+    this.html(this.view('age')({
+      year:         majorMinor[0],
+      milliseconds: majorMinor[1]
+    }));
+  }.bind(this));
+};
+
+App.fn.$$ = function(sel){
+  return this.$el.querySelectorAll(sel);
+};
+
+App.fn.html = function(html){
+  this.$el.innerHTML = html;
+};
+
+App.fn.view = function(name){
+  var $el = $(name + '-template');
+  return Handlebars.compile($el.innerHTML);
+};
+
+window.app = new App($('app'))
+
+})();
